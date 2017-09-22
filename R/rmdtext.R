8de6e744ac2164237ecc82f915c369f8b75f7c9e
Antoine Neuraz <antoine.neuraz@gmail.com>
2017-09-22 11:27:15 +0200

    rebase

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -4,7 +4,7 @@
 #' @export
 rmdtext <- function(text){
   writeLines(text, con="input.Rmd");
-  rmarkdown::render("input.Rmd", output_file="output.pdf");
+  rmarkdown::render("input.Rmd", output_file="output.html");
   invisible();
 }
 
